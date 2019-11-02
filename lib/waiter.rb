361234ddd325d0ed4eb8c2e13f21543a6d409bb4
github username <github email address>
2019-11-02 16:45:49 -0700

    Done.

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,33 @@
-class Waiter
+require 'pry'
 
+class Waiter
+    attr_reader :name, :years_of_experience
+
+    @@all = []
+
+    def self.all
+        @@all
+    end
+    
+    def initialize(name, years_of_experience)
+        @name = name
+        @years_of_experience = years_of_experience
+
+        @@all << self
+    end
+
+    def new_meal(customer, total, tip)
+        Meal.new(self, customer, total, tip)
+    end
+
+    def meals
+        Meal.all.select{|meal| meal.waiter == self}
+    end
+
+    def best_tipper
+        best_tipper = meals.max do |x, y|
+            x.tip <=> y.tip
+        end
+        best_tipper.customer
+    end
 end
